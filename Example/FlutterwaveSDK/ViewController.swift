baf70b150b8b3d8c7be96475b941f21fd9e7f86f
Texyz <34765619+texyz@users.noreply.github.com>
2020-12-01 10:55:21 +0100

    Save Cards

--- Example/FlutterwaveSDK/ViewController.swift
+++ Example/FlutterwaveSDK/ViewController.swift
@@ -30,30 +30,30 @@ class ViewController: UIViewController, FlutterwavePayProtocol {
     let launchButton = UIButton(type: .system)
     
     
-    
     @objc func showExample(){
+        
         let config = FlutterwaveConfig.sharedConfig()
-
+        
         config.paymentOptionsToExclude = []
-        config.currencyCode = "XAF" // This is the specified currency to charge in.
-        config.email = "texy77@gmail.com" // This is the email address of the customer
-        config.isStaging = false// Toggle this for staging and live environment
-        config.phoneNumber = "07065362811" //Phone number
-        config.transcationRef = "IOS-TESTT" // This is a unique reference, unique to the particular transaction being carried out. It is generated when it is not provided by the merchant for every transaction.
+        config.currencyCode = "NGN" // This is the specified currency to charge in.
+        config.email = "user@flw.com" // This is the email address of the customer
+        config.isStaging = false // Toggle this for staging and live environment
+        config.phoneNumber = "07067783334" //Phone number
+        config.transcationRef = "IOS-TEST" // This is a unique reference, unique to the particular transaction being carried out. It is generated when it is not provided by the merchant for every transaction.
         config.firstName = "Yemi" // This is the customers first name.
         config.lastName = "Desola" //This is the customers last name.
         config.meta = [["metaname":"sdk", "metavalue":"ios"]] //This is used to include additional payment information
         config.narration = "simplifying payments for endless possibilities"
-        config.publicKey = "FLWPUBK-aa4cd0b443404147d2d8229a37694b00-X" //Public key
-        config.encryptionKey = "c9a8f6a1a8bd45f850351621" //Encryption key
+        config.publicKey = "[PUB_KEY]" //Public key
+        config.encryptionKey = "[ENCRYPTION_KEY]" //Encryption key
         config.isPreAuth = false  // This should be set to true for preauthoize card transactions
         let controller = FlutterwavePayViewController()
         let nav = UINavigationController(rootViewController: controller)
-        controller.amount = "10" // This is the amount to be charged.
+        controller.amount = "[Amount]" // This is the amount to be charged.
         controller.delegate = self
         self.present(nav, animated: true)
+        
     }
-    
     
     
     override func viewDidLoad() {
